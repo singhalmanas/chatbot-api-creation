2a4e8610016291154759ca46135c2544673c95e8
Manas Singhal <singhal.manas@gmail.com>
2019-10-09 19:20:26 +0530

    Fixed bug

--- chatbot-api-data-layer/CRUDDataLayer.cs
+++ chatbot-api-data-layer/CRUDDataLayer.cs
@@ -8,25 +8,25 @@ namespace ChatbotApiDataLayer
     public class CRUDDataLayer
     {
         string connectionString = "Data Source=chatbot-data.database.windows.net;User ID = chatbot-user; Password=Noida@144;Connect Timeout = 30; Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-         
+
         //string conStr = @"Server=sql-elastic-pool.database.windows.net;Integrated security=SSPI;database=";
         SqlConnection myConn = new SqlConnection();
         public bool IsUserExists(User user)
         {
             bool isExists = false;
             try
-            {               
-                connectionString =  connectionString + ";database=" + user.DataBaseName;
+            {
+                connectionString = connectionString + ";database=" + user.DataBaseName;
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    var commandStr = "select * from information_schema.tables where table_name =user"; 
+                    var commandStr = "select * from information_schema.tables where table_name =user";
 
                     using (SqlCommand cmd = new SqlCommand(commandStr, con))
                     {
                         using (SqlDataReader reader = cmd.ExecuteReader())
                         {
-                            isExists= reader.HasRows;
+                            isExists = reader.HasRows;
                         }
                     }
                     con.Close();
@@ -47,7 +47,7 @@ namespace ChatbotApiDataLayer
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    var commandStr = "select StoreId from StoreType where storename='"+storetype+"'";
+                    var commandStr = "select StoreId from StoreType where storename='" + storetype + "'";
                     using (SqlCommand cmd = new SqlCommand(commandStr, con))
                     {
                         using (SqlDataReader reader = cmd.ExecuteReader())
@@ -56,7 +56,7 @@ namespace ChatbotApiDataLayer
                             {
                                 while (reader.Read())
                                 {
-                                    storeTypeId= Convert.ToInt32(reader["storename"]);
+                                    storeTypeId = Convert.ToInt32(reader["storename"]);
                                 }
                             }
                         }
@@ -81,7 +81,7 @@ namespace ChatbotApiDataLayer
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    var commandStr = "INSERT INTO companyinfo (DBName,Uname,StoreTypeId) VALUES ('"+storename+"','"+username+"',"+storetype+")";
+                    var commandStr = "INSERT INTO companyinfo (DBName,Uname,StoreTypeId) VALUES ('" + storename + "','" + username + "'," + storetype + ")";
                     using (SqlCommand cmd = new SqlCommand(commandStr, con))
                     {
                         var result = cmd.ExecuteNonQuery();
@@ -94,7 +94,7 @@ namespace ChatbotApiDataLayer
             {
                 throw;
             }
-            
+
         }
 
         public bool IsUserExistsForStore(string username, string storename, string storetype)
@@ -102,11 +102,11 @@ namespace ChatbotApiDataLayer
             bool isExists = false;
             try
             {
-                connectionString = connectionString + ";database=chatbot-master" ;
+                connectionString = connectionString + ";database=chatbot-master";
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
                     con.Open();
-                    var commandStr = "select * from companyinfo cf join storetype st on cf.storetypeid=st.storetypeid where dbname='"+storename+"' and username='"+username+"' and storetype='"+storetype+"'";
+                    var commandStr = "select * from companyinfo cf join storetype st on cf.storetypeid=st.storetypeid where dbname='" + storename + "' and username='" + username + "' and storetype='" + storetype + "'";
                     using (SqlCommand cmd = new SqlCommand(commandStr, con))
                     {
                         using (SqlDataReader reader = cmd.ExecuteReader())
@@ -138,7 +138,7 @@ namespace ChatbotApiDataLayer
                     {
                         using (SqlDataReader reader = cmd.ExecuteReader())
                         {
-                            result = reader.HasRows? "User Created": result;
+                            result = reader.HasRows ? "User Created" : result;
                         }
                     }
                     con.Close();
@@ -163,14 +163,12 @@ namespace ChatbotApiDataLayer
                 {
                     con.Open();
                     var commandStr = "CREATE DATABASE " + database;
-                    
+
                     using (SqlCommand cmd = new SqlCommand(commandStr, con))
                     {
                         cmd.CommandTimeout = 300;
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            result = reader.HasRows? "DataBase Created": result;
-                        }
+                        cmd.ExecuteNonQuery();
+                        result = "DataBase Created";
                     }
                     con.Close();
                 }
