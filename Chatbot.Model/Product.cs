42b5c67d34ec28e4b966398c394019ce03ff42e0
Sagar Sati <sagar.sati@sapient.com>
2019-10-09 12:44:22 +0530

    - Added id

--- Chatbot.Model/Product.cs
+++ Chatbot.Model/Product.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ChatbotAPI.Model
 {
     public class Product
     {
+        [Key]
+        public string ProductId { get; set; }
         public string ProductName { get; set; }
         public string Quantity { get; set; }
         public string Price { get; set; }
+        public string Discount { get; set; }
     }
 }
--- Chatbot.Model/User.cs
+++ Chatbot.Model/User.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ChatbotAPI.Model
 {
     public class User
     {
+        [Key]
+        public string UserID { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public string LastName { get; set; }
